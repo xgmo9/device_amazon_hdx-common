75d0aed49c68bed33466d5f5524943ec32c782a4
Joel Bacchus <joel.bacchus@protonmail.com>
2020-06-16 15:09:51 +0100

    media: move xml configuration files to new location

--- hdx-common.mk
+++ hdx-common.mk
@@ -67,9 +67,9 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/etc/media_profiles.xml:system/etc/media_profiles.xml \
-	$(LOCAL_PATH)/configs/etc/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/configs/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
