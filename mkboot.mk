b51bd15e4bb4df30c1453bce9a62bc7712e0fc8d
Joel Bacchus <joel.bacchus@protonmail.com>
2020-09-05 20:48:30 +0100

    mkboot: update for los-16.0

--- mkboot.mk
+++ mkboot.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 ## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolLineage$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 LZMA := $(shell which lzma)
 
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_HEADERS) $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
 	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
 	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
@@ -24,9 +24,9 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(recovery_uncompressed_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"Building hdx hacked recovery image: $@"${CL_RST}
-	$(hide) rm -f $(OUT)/ramdisk-recovery.cpio.lzma
+	$(hide) rm -f $(OUT_DIR)/ramdisk-recovery.cpio.lzma
 	@echo -e ${CL_CYN}"Compressing recovery ramdisk..."${CL_RST}
-	$(hide) $(LZMA) -z -9 $(recovery_uncompressed_ramdisk)
+	$(hide) $(LZMA) -f -z -9 $(recovery_uncompressed_ramdisk)
 	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(COMMON_FOLDER)/pyCuber/pyCuber.sh $@
