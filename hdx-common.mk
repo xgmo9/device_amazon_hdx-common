f19f8f46ab682f96a9cd22adb91539f3e4c3ac8a
Joel Bacchus <joel.bacchus@protonmail.com>
2020-09-28 08:58:04 +0100

    wifi: commonise configuration files

--- hdx-common.mk
+++ hdx-common.mk
@@ -194,6 +194,12 @@ PRODUCT_PACKAGES += \
     libwpa_client \
 	wificond
 
+# WiFi Config
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+
 # Misc dependencies
 PRODUCT_PACKAGES += \
     ebtables \
